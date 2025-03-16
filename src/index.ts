6774ec4ec6db31aa7cfe941040faa4e939280a27
GabrielCatanzaro <gabrielgomescatanzaro@gmail.com>
2025-03-16 16:48:24 -0300

    Primeiro exercicio feito

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// Exercício 1
+interface Carro {
+    marca: string;
+    modelo: string;
+    ano: number;
+    motor?: string;
+}
+
+const meuCarro: Carro = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    ano: 2022,
+    motor: "1.8 Flex"
+};
+
+console.log(meuCarro);
