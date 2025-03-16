9ab6a255c92fcaf061e309318d5f375bf2849eef
GabrielCatanzaro <gabrielgomescatanzaro@gmail.com>
2025-03-16 16:49:39 -0300

    Segundo e terceriro exercicios finalizados

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,21 @@ const meuCarro: Carro = {
 };
 
 console.log(meuCarro);
+
+// Exercício 2
+interface Multiplicacao {
+    (a: number, b: number): number;
+}
+
+const multiplicar: Multiplicacao = (a, b) => a * b;
+
+console.log(multiplicar(5, 3));
+console.log(multiplicar(10, 2));
+
+// Exercício 3
+function inverterArray<T>(array: T[]): T[] {
+    return array.reverse();
+}
+
+console.log(inverterArray([1, 2, 3, 4]));
+console.log(inverterArray(["a", "b", "c", "d"]));
